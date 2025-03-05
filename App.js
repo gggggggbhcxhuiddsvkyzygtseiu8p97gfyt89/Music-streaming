2b7415d149dc638412278b846a5627167316e0b6
gggggggbhcxhuiddsvkyzygtseiu8p97gfyt89 <167583793+gggggggbhcxhuiddsvkyzygtseiu8p97gfyt89@users.noreply.github.com>
2025-03-05 15:35:00 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+    return (
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+            <Footer />
+        </Router>
+    );
+}
+
+export default App;
